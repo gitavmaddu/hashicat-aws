2d4c350ca254eb1fb1de563451754a66274e414c
Atm aMAddula <atma.maddula@gmail.com>
2021-03-30 18:17:47 +0000

    added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/AtmaOrg/s3-bucket/aws"
   version = "1.15.0"
   # insert required variables here
-  bucket_prefix = prefix
+  bucket_prefix = "atma"
 }
